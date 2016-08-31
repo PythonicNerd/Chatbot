b383dff573b6351519bbc0e15f738e69b15e21c3
PythonicNerd <neilaylor@hotmail.com>
2016-08-30 20:23:44 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 import random
 import time
 
-class Bot:
+class Bot: #This will be our main class that will hold all that we need for the bot
+  
+  def __init__(self):
+    self.data = open("knowledge","r+")
+    #getting info about the user
+    self.name = ""
+    self.age=""
+    self.fv_color=""
+    
+    #geting info from file
+    for line in data.readlines():
+      key = line[0:2].lower()
+      if key == "fvc":
+        self.fv_color = line[2:len(line)]
+    
